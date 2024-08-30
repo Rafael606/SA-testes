80bd4d91d5a64823d88d50f09229ac833bac1297
Rafael606 <156861730+Rafael606@users.noreply.github.com>
2024-08-29 21:29:31 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const api = express();
+const porta = 80;
+
+api.get('/', (req, res) => {
+    const rotaPadrao = {
+        nome_rota: '/',
+        codigo_status: '200',
+        metodo: 'GET'
+    };
+
+    res.status(200).json(rotaPadrao);
+});
+
+// Cria clientes
+api.post('/clientes', (req, res) => {
+    const response = [
+        {
+            mensagem: 'Cliente criado com sucesso',
+            status: 201
+        }
+    ];
+
+    res.status(201).json(response);
+});
+
+api.listen(porta, () => {
+    console.log(`Servidor rodando na porta ${porta}`);
+});
